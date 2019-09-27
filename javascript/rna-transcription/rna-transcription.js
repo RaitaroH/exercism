696a252c174ca70b83c81acfc896ddcddc7d81bd
RaitaroHikami <19552720+RaitaroH@users.noreply.github.com>
2019-09-27 21:49:44 +0300

    dna to rna improvement

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -3,15 +3,13 @@
 // convenience to get you started writing code faster.
 //
 
-let res = '';
+let res = ''; // this makes the if not needed for the empty test
 export const toRna = (str) => {
-  if (str) {
     res = str.replace(/G/g, "c"); // avoid further replacements
     res = res.replace(/C/g, "g");
     res = res.replace(/T/g, "a");
     res = res.replace(/A/g, "u");
     res = res.toUpperCase(); // return capital letters now
     return(res);
-  } else { return(str); };
 };
 
