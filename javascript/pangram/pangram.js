f39affcf5eeb19af388cf4fa94892c67b6300c64
RaitaroHikami <19552720+RaitaroH@users.noreply.github.com>
2019-10-01 13:16:06 +0300

    js: pangram, ;

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,9 +1,9 @@
-let letters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+let letters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
 
 export const isPangram = (string) => {
   for(let i=0; i< letters.length; i++) {
     var letter = letters[i];
     if( string.toLowerCase().includes(letter) == false ) return(false);
     if( letter == 'z') return(true); // prev if didn't pass so z is in the string
-  }
+  };
 };
